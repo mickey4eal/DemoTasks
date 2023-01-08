1e11d807b81edda8ac731bef55a048e4c272b1ce
Michael O <michaelobikwee@yahoo.co.uk>
2023-01-08 21:37:23 +0000

    Simple Approach to WOrking with Lists/Arrays/Enumarables

--- CareerPassportDemo/Program.cs
+++ CareerPassportDemo/Program.cs
@@ -1,6 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Claims;
+
 var printerService = new PrinterService();
 var mathsOperator = new SimpleMathsOperations();
+var classRegister = new ClassRegister();
 printerService.Print("Hello, World!");
 
 var numA = 7.5;
@@ -13,6 +16,11 @@ printerService.Print($"Division of {numA} and {numB} is {mathsOperator.DivideNum
 printerService.Print($"Square of {numA} is {mathsOperator.SquareNumber(numA)} \n");
 printerService.Print($"Square of {numB} is {mathsOperator.SquareNumber(numB)} \n");
 
+classRegister.AddStudent(student: "Grace Jones");
+classRegister.AddStudents(students: new string[] { "Ronald Sterling", "Dax Jefferson", "Arinze Chukwu", "Don Draper", "Peggy Sue" });
+var students = classRegister.GetStudents();
+printerService.Print($"There are {students.Count()} registered in the class.\nHere are their names:\n");
+students.ToList().ForEach(x => printerService.Print($"{x}\n"));
 
 public class PrinterService
 {
@@ -29,4 +37,20 @@ public class SimpleMathsOperations
     public double MultiplyNumbers(double x, double y) => x * y;
     public double DivideNumbers(double x, double y) => x / y;
     public double SquareNumber(double x) => x * x;
+}
+
+//Level 1
+//- Working with Arrays/ Lists: Student / Class Room Register => Record given values and print in console
+public class ClassRegister
+{
+    private readonly List<string> _students;
+
+    public ClassRegister()
+    {
+        _students = new List<string>();
+    }
+
+    public void AddStudent(string student) => _students.Add(student);
+    public void AddStudents(IEnumerable<string> students) => _students.AddRange(students);
+    public IEnumerable<string> GetStudents() => _students;
 }
---
+++ CareerPassportDemoTests/ClassRegisterTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace Tests
+{
+    [TestClass()]
+    public class ClassRegisterTests
+    {
+        private ClassRegister? _classRegister;
+
+        [TestMethod()]
+        public void AddStudentTest()
+        {
+            //Arrange
+            _classRegister = new();
+
+            //Act
+            _classRegister.AddStudent("Student A");
+            var result = _classRegister.GetStudents();
+
+            //Assert
+            Assert.AreEqual(1, result.Count());
+            Assert.AreEqual("Student A", result.First());
+        }
+
+        [TestMethod()]
+        public void AddStudentsTest()
+        {
+            //Arrange
+            _classRegister = new();
+
+            //Act
+            _classRegister.AddStudents(new string[] { "Student A" , "Student B" , "Student C" });
+            var result = _classRegister.GetStudents();
+
+            //Assert
+            Assert.AreEqual(3, result.Count());
+            Assert.AreEqual("Student C", result.Last());
+        }
+    }
+}
