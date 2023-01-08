a575aacc9432eabb81ff7314ff934cec9d074094
Michael O <michaelobikwee@yahoo.co.uk>
2023-01-08 20:26:35 +0000

    Add project files.

---
+++ CareerPassportDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+var printerService = new PrinterService();
+printerService.Print("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+
+public class PrinterService
+{
+    public void Print(object word)
+    {
+        Console.WriteLine(word);
+    }
+}
+
+public class SimpleMathsOperations
+{
+    public double AddNumbers(double x, double y) => x + y;
+    public double SubtractNumbers(double x, double y) => x - y;
+    public double MultiplyNumbers(double x, double y) => x * y;
+    public double DivideNumbers(double x, double y) => x / y;
+    public double SquareNumber(double x) => x * x;
+}
---
+++ CareerPassportDemoTests/PrinterServiceTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestPlatform.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CareerPassportDemoTests
+{
+    [TestClass()]
+    public class PrinterServiceTests
+    {
+        private PrinterService _helloWorld;
+
+        public PrinterServiceTests()
+        {
+            _helloWorld = new ();
+        }
+
+        [TestMethod()]
+        public void PrintWordTest()
+        {
+            var currentConsoleOut = Console.Out;
+
+            using (var consoleOutput = new ConsoleOutput())
+            {
+                _helloWorld.Print("Hello, World!");
+
+                Assert.AreEqual("Hello, World!\r\n", consoleOutput.GetOuput());
+            }
+
+            Assert.AreEqual(currentConsoleOut, Console.Out);
+        }
+    }
+}
