8d0bfd40d8f16605168da85cb06b7c9e4fcf8497
Michael O <michaelobikwee@yahoo.co.uk>
2023-01-21 14:50:47 +0000

    Made some quick conversions to TS and JS. More work to be done.

---
+++ CareerPassportDemo/Scripts/JSClassConversions.jsx
@@ -0,0 +1,61 @@
+﻿//JavaScript
+"use strict";
+exports.__esModule = true;
+var PrinterService = /** @class */ (function () {
+    function PrinterService() {
+    }
+    PrinterService.prototype.Print = function (word) { };
+    return PrinterService;
+}());
+exports.PrinterService = PrinterService;
+
+
+"use strict";
+exports.__esModule = true;
+var SimpleMathsOperations = /** @class */ (function () {
+    function SimpleMathsOperations() {
+    }
+    SimpleMathsOperations.prototype.AddNumbers = function (x, y) {
+        return x + y;
+    };
+    SimpleMathsOperations.prototype.SubtractNumbers = function (x, y) {
+        return x - y;
+    };
+    SimpleMathsOperations.prototype.MultiplyNumbers = function (x, y) {
+        return x * y;
+    };
+    SimpleMathsOperations.prototype.DivideNumbers = function (x, y) {
+        return x / y;
+    };
+    SimpleMathsOperations.prototype.SquareNumber = function (x) {
+        return x * x;
+    };
+    return SimpleMathsOperations;
+}());
+exports.SimpleMathsOperations = SimpleMathsOperations;
+
+
+"use strict";
+exports.__esModule = true;
+var ClassRegister = /** @class */ (function () {
+    function ClassRegister() {
+    }
+    ClassRegister.prototype.AddStudent = function (student) { };
+    ClassRegister.prototype.AddStudents = function (students) { };
+    ClassRegister.prototype.GetStudents = function () { };
+    return ClassRegister;
+}());
+exports.ClassRegister = ClassRegister;
+
+
+"use strict";
+exports.__esModule = true;
+var Multiples = /** @class */ (function () {
+    function Multiples() {
+    }
+    Multiples.prototype.SumOfMultiples = function (n, max) {
+        return 1;
+    };
+    return Multiples;
+}());
+exports.Multiples = Multiples;
---
+++ CareerPassportDemo/Scripts/TSClassConversions.ts
@@ -0,0 +1,37 @@
+﻿//TypeScript
+export class PrinterService {
+    public Print(word: string | null): void { }
+}
+
+export class SimpleMathsOperations {
+    public AddNumbers(x: number, y: number): number {
+        return x + y;
+    }
+    public SubtractNumbers(x: number, y: number): number {
+        return x - y;
+    }
+    public MultiplyNumbers(x: number, y: number): number {
+        return x * y;
+    }
+    public DivideNumbers(x: number, y: number): number {
+        return x / y;
+    }
+    public SquareNumber(x: number): number {
+        return x * x;
+    }
+}
+
+
+import { TypeInfo } from "@brandless/tsutility";
+export class ClassRegister {
+    constructor() { }
+    public AddStudent(student: string | null): void { }
+    public AddStudents(students: string | null): void { }
+    public GetStudents(): void { }
+}
+
+export class Multiples {
+    public SumOfMultiples(n: number, max: number): number {
+        return 1;
+    }
+}
